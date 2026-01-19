c37be5ee728875e7b11d8035166f94c0b343b68e
rshghb0206 <bgp9jc5m9c@privaterelay.appleid.com>
2026-01-19 18:38:21 +0900

    [프로그래머스] L0 - 몫 구하기 (#7)

---
+++ programmers/L0_기초입문/입문/007_몫_구하기.py
@@ -0,0 +1,9 @@
+# 몫 구하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120805
+# 알고리즘: 기초
+# 작성자: 유승희
+# 작성일: 2026. 01. 19. 18:38:20
+
+def solution(num1, num2):
+    return num1//num2
