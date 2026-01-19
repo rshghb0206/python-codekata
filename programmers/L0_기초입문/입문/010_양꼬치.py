8bef0a06e956e171c90f460e46ac909ff17d5ad3
rshghb0206 <bgp9jc5m9c@privaterelay.appleid.com>
2026-01-19 18:51:53 +0900

    [프로그래머스] L0 - 양꼬치 (#10)

---
+++ programmers/L0_기초입문/입문/010_양꼬치.py
@@ -0,0 +1,10 @@
+# 양꼬치
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120830
+# 알고리즘: 기초
+# 작성자: 유승희
+# 작성일: 2026. 01. 19. 18:51:52
+
+def solution(n, k):
+    n_f=n//10
+    return 12000*n + 2000*(k-n_f)
